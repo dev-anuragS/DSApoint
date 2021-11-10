8205f6ea70cec973e009d3f8382c5faf779b0178
Anurag <anurag.astikint@gmail.com>
2021-11-10 19:45:06 +0530

    home page modified

--- dsapoint/src/components/Home.jsx
+++ dsapoint/src/components/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import '../components/sass/home.scss'
 
 const Home = () => {
     return (
-        <div>
-            HOME
-        </div>
+        <>
+            <div id="home">
+                yolo
+            </div>
+        </>
     )
 }
 
