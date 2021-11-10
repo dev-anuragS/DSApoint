fdd3095ae38f1ebba4e0d385c3e0d97d702e06f5
Anurag <anurag.astikint@gmail.com>
2021-11-10 19:21:52 +0530

    added sass

--- dsapoint/src/index.js
+++ dsapoint/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './components/css/main.css'
+import './components/sass/main.scss';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 
