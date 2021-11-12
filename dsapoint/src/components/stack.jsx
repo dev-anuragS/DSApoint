b360d2c7cbf26d57a44426f8b41c55d29eb890bc
Ankur_Tyagi <ankurtyagi557788@gmail.com>
2021-11-12 18:27:00 +0530

    modified Stack.jsx and stack.scss

--- dsapoint/src/components/stack.jsx
+++ dsapoint/src/components/stack.jsx
@@ -1,12 +1,50 @@
 // Name of COmponents start with Capital
 
 import React from 'react'
+import '../components/sass/stack.scss'
 
 const Stack = () => {
     return (
         <>
-            <div>
-                This is a div for stack
+            <h2>Stack Data Structure </h2>
+            <hr />
+            <div id="stackcontent">
+                <div id="theory">
+                    <div>
+
+                    </div>
+                    <p>Stack is a linear data structure which follows a particular order in which the operations are performed. The order may be LIFO(Last In First Out) or FILO(First In Last Out)</p>
+                    {/* insert an image */}
+                    <p>There are many real-life examples of a stack. Consider an example of plates stacked over one another in the canteen. The plate which is at the top is the first one to be removed, i.e. the plate which has been placed at the bottommost position remains in the stack for the longest period of time. So, it can be simply seen to follow LIFO(Last In First Out)/FILO(First In Last Out) order.</p>
+                    <h3>Basic Operation</h3>
+                    <p>Stack operations may involve initializing the stack, using it and then de-initializing it. Apart from these basic stuffs, a stack is used for the following two primary operations −</p>
+                    <ul>
+                        <li><strong>push() </strong> − Pushing (storing) an element on the stack.</li>
+                        <li><strong>pop() </strong> − Removing (accessing) an element from the stack.</li>
+                    </ul>
+                    <p>To use a stack efficiently, we need to check the status of stack as well. For the same purpose, the following functionality is added to stacks −</p>
+                    <ul>
+                        <li><strong>peek() </strong> − get the top data element of the stack, without removing it</li>
+                        <li><strong>isFull() </strong>  − check if stack is full</li>
+                        <li><strong>isEmpty() </strong>isEmpty() − check if stack is empty.</li>
+                    </ul>
+                    <p>At all times, we maintain a pointer to the last PUSHed data on the stack. As this pointer always represents the top of the stack, hence named top. The top pointer provides top value of the stack without actually removing it.</p>
+                </div>
+                <div id="animation">
+                    <div id="infinite">
+                        <div className="mainStack">
+                            <div className="elem" id="firstElem">1</div>
+                            <div className="elem" id="secElem">2</div>
+                            <div className="elem">3</div>
+                            <div className="elem">4</div>
+                        </div>
+                        <p><strong>Stack</strong></p>
+                    </div>
+
+                    <div id="controlled">
+                        VISUALISE
+                    </div>
+                </div>
             </div>
         </>
     )
