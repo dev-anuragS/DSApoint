fe2f682d1dc4f47ffff25bab0d6d34b11d11b6a8
Anurag <anurag.astikint@gmail.com>
2021-11-12 22:37:10 +0530

    Updated home.jsx and home.scss

--- dsapoint/src/components/Home.jsx
+++ dsapoint/src/components/Home.jsx
@@ -7,11 +7,11 @@ const Home = () => {
             <div id="home">
                 <div id="homeContent">
                     <div id="homeTitle">
-                        <h1>dp</h1>
-                        <p>DSApoint</p>
+                        <h1 id='logo'>dp</h1>
+                        <p id='logoName'>DSApoint</p>
                     </div>
                     <div id='homeScreenContent'>
-                        <p>A DSA Learning and Coding Platform</p>
+                        <p id='logoContent'>A DSA Learning and Coding Platform</p>
                     </div>
                 </div>
             </div>
