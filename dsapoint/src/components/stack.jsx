5956ab39fdb0a48f46f8811aaa2cc1cf3ad8026e
Ankur_Tyagi <ankurtyagi557788@gmail.com>
2021-11-13 13:37:08 +0530

    Added infinite animation for stack and modified card

--- dsapoint/src/components/stack.jsx
+++ dsapoint/src/components/stack.jsx
@@ -26,24 +26,51 @@ const Stack = () => {
                     <ul>
                         <li><strong>peek() </strong> − get the top data element of the stack, without removing it</li>
                         <li><strong>isFull() </strong>  − check if stack is full</li>
-                        <li><strong>isEmpty() </strong>isEmpty() − check if stack is empty.</li>
+                        <li><strong>isEmpty() </strong> − check if stack is empty.</li>
                     </ul>
                     <p>At all times, we maintain a pointer to the last PUSHed data on the stack. As this pointer always represents the top of the stack, hence named top. The top pointer provides top value of the stack without actually removing it.</p>
                 </div>
                 <div id="animation">
                     <div id="infinite">
                         <div className="mainStack">
-                            <div className="elem" id="firstElem">1</div>
-                            <div className="elem" id="secElem">2</div>
+                            <div className="elem" id="firstElem">1
+                                <div id="pop">pop </div>
+                                <div id="push">push </div>
+                            </div>
+                            <div className="elem">2</div>
                             <div className="elem">3</div>
                             <div className="elem">4</div>
                         </div>
                         <p><strong>Stack</strong></p>
                     </div>
 
-                    <div id="controlled">
-                        VISUALISE
-                    </div>
+                    <div class="maincontainer">
+        <div class="thecard">
+            <div class="thefront">
+                <div id="dp">
+                    <h1>dp</h1>
+                </div>
+                <div id="content">
+                    <ul>
+                        <li>Understanding Stack is difficult!</li>
+                        <li>Not anymore, Come Here</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="theback">
+                <div id="visualizebtn">Visualize</div>
+                <div id="visualContent">
+                    <h3>Stack Operations</h3>
+                    <ul>
+                        <li>Pop</li>
+                        <li>push</li>
+                        <li>peek</li>
+                        <li>top</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
                 </div>
             </div>
         </>
