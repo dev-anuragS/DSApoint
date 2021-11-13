d099ff72022b7e94577326f053b0c7b4a35e4cc0
Anurag <anurag.astikint@gmail.com>
2021-11-13 20:14:16 +0530

    Login.jsx and login.scss created

--- dsapoint/src/App.js
+++ dsapoint/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import Stack from './components/Stack'
+import Login from './components/Login'
 import {Switch,Route} from 'react-router-dom'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar/>
       <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/login' component={Login}/>
         <Route exaxt path='/stack' component = {Stack}/>
       </Switch>
       <Footer/>
---
+++ dsapoint/src/components/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import '../components/sass/login.scss'
+
+const Login = () => {
+    return (
+        <>
+            <div id="container">
+                <div id="loginContainer">
+                    <div id="left">
+                        left
+                    </div>
+                    <div id="right">
+                        right
+                    </div>
+                </div>
+            </div>
+
+        </>
+)}
+
+export default Login
