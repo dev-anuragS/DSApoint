ec4fb436d236bbdc6fede4440a7f7a173b742d30
Anurag <anurag.astikint@gmail.com>
2021-11-14 17:34:04 +0530

    Changed Footer.jsx, footer.scss, Login.jsx, login.scss and home.scss

--- dsapoint/src/components/Footer.jsx
+++ dsapoint/src/components/Footer.jsx
@@ -12,62 +12,62 @@ const Footer = () => {
 
     return (
         <div>
-            <footer class="bg-dark text-center text-white footerCss">
-                <div class="container p-4">
-                    <section class="mb-4">
-                        <NavLink class="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-                        ><i class="fab fa-facebook-f"></i
+            <footer className="bg-dark text-center text-white footerCss">
+                <div className="container p-4">
+                    <section className="mb-4">
+                        <NavLink className="btn btn-outline-light btn-floating m-1 socialLogo" exact to="#!" role="button"
+                        ><i className="fab fa-facebook-f"></i
                         ></NavLink>
 
-                        <NavLink class="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-                        ><i class="fab fa-twitter"></i
+                        <NavLink className="btn btn-outline-light btn-floating m-1 socialLogo" exact to="#!" role="button"
+                        ><i className="fab fa-twitter"></i
                         ></NavLink>
 
-                        <NavLink class="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-                        ><i class="fab fa-google"></i
+                        <NavLink className="btn btn-outline-light btn-floating m-1 socialLogo" exact to="#!" role="button"
+                        ><i className="fab fa-google"></i
                         ></NavLink>
 
-                        <NavLink class="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-                        ><i class="fab fa-instagram"></i
+                        <NavLink className="btn btn-outline-light btn-floating m-1 socialLogo" exact to="#!" role="button"
+                        ><i className="fab fa-instagram"></i
                         ></NavLink>
 
-                        <NavLink class="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-                        ><i class="fab fa-linkedin-in"></i
+                        <NavLink className="btn btn-outline-light btn-floating m-1 socialLogo" exact to="#!" role="button"
+                        ><i className="fab fa-linkedin-in"></i
                         ></NavLink>
 
-                        <NavLink class="btn btn-outline-light btn-floating m-1" to="#!" role="button"
-                        ><i class="fab fa-github"></i
+                        <NavLink className="btn btn-outline-light btn-floating m-1 socialLogo" exact to="#!" role="button"
+                        ><i className="fab fa-github"></i
                         ></NavLink>
                     </section>
-                    <section class="">
+                    <section className="">
                         <form action="" onSubmit={()=>alert(emailData)}>
-                            <div class="row d-flex justify-content-center">
+                            <div className="row d-flex justify-content-center">
                                 
-                                <div class="col-auto">
-                                    <p class="pt-2">
+                                <div className="col-auto">
+                                    <p className="pt-2">
                                         <strong>Sign up for our newsletter</strong>
                                     </p>
                                 </div>
-                                <div class="col-md-5 col-12">
-                                    <div class="form-outline form-white mb-4">
-                                        <input type="email" id="form5Example2" class="form-control" value={emailData} onChange={inputEvent} placeholder="Write your Email here"/>
+                                <div className="col-md-5 col-12">
+                                    <div className="form-outline form-white mb-4">
+                                        <input type="email" id="form5Example2" className="form-control" value={emailData} onChange={inputEvent} placeholder="Write your Email here"/>
                                     </div>
                                 </div>
-                                <div class="col-auto">
-                                    <button type="submit" class="btn btn-outline-light mb-4">
+                                <div className="col-auto">
+                                    <button type="submit" className="btn btn-outline-light mb-4">
                                         Subscribe
                                     </button>
                                 </div>
                             </div>
                         </form>
                     </section>
-                    <section class="mb-4">
+                    <section className="mb-4">
                         <p>
                             To get regular updates about new content and courses fill the above E-Mail field and you'll recieve regular updates.
                         </p>
                     </section>
                 </div>
-                <div class="text-center p-3">
+                <div className="text-center p-3">
                     © 2020 Copyright: DSApoint
                 </div>
             </footer>
--- dsapoint/src/components/Login.jsx
+++ dsapoint/src/components/Login.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 import '../components/sass/login.scss'
 
 const Login = () => {
+
+    const [loginData,setLoginData] = useState({
+        emailId:"",
+        password:""
+    });
+
+    const inputEvent = (event) =>{
+        const name=event.target.name;
+        const value=event.target.value;
+        console.log(name+" "+value);
+        setLoginData({
+            ...loginData,
+            [name]: value
+        })
+    }
+
     return (
         <>
             <div id="container">
@@ -13,10 +30,10 @@ const Login = () => {
                     </div>
                     <div id="right">
                         <form action="" id="loginForm">
-                            <label htmlFor="emailId"><span class='smallTextLoginForm'>Email</span></label>
-                            <input type="email" name="emailId" id="emailId" value='' />
-                            <label htmlFor="password"><span class='smallTextLoginForm'>Password</span></label>
-                            <input type="password" name="password" id="password" value='' />
+                            <label htmlFor="emailId"><span className='smallTextLoginForm'>Email</span></label>
+                            <input type="email" name="emailId" id="emailId" value={loginData.emailId} onChange={inputEvent} />
+                            <label htmlFor="password"><span className='smallTextLoginForm'>Password</span></label>
+                            <input type="password" name="password" id="password" value={loginData.password} onChange={inputEvent} />
                             <button type="submit" id='loginFormBtn'>Submit</button>
                         </form>
                     </div>
